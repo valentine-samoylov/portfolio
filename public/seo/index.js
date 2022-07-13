a78870d09efabeff33c176de7018249d23cd0d96
Valentine Samoylov <valentine.samoylov@gmail.com>
2022-07-13 21:15:37 +0300

    SEO have been updated.

--- public/seo/index.js
+++ public/seo/index.js
@@ -3,6 +3,6 @@ module.exports = {
   title: 'Valentine Samoylov - Front-end developer',
   description:
     'A motivated front-end developer with a passion for coding, building websites and web applications. I work with React and JavaScript with SASS preprocessor and style libraries like Tailwind CSS.',
-  previewImg: 'assets/og-preview.png',
+  previewImg: '../public/og-preview.png',
   url: 'https://valentine-samoylov.vercel.app',
 }
